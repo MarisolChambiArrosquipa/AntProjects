cafd41d19f7a684d3572d45fffbcaeae7899de6f
marisolChambiArrosquipa <marysol_m5@hotmail.com>
2015-11-15 08:48:35 -0400

    Ant with junit and Selenium webDriver

---
+++ AntExamplesProject/src/com/myproject/test/AntwihJunit.java
@@ -0,0 +1,40 @@
+package com.myproject.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class AntwihJunit {
+	String testname;
+	WebDriver driver;
+	@Before
+	public void setup(){
+		System.out.println("run setup method");
+		testname=this.toString();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sol\\Desktop\\Project_Todo\\broswer\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		
+	}
+		@Test
+     	public void testMethod(){
+		driver.get("https://www.google.com.bo");
+		ImplicitExplictWaits.ImplicitWait(driver);
+		SearchGooglePageObject search=new SearchGooglePageObject(driver);
+		search.txtSearch.sendKeys("Selenium");
+	    search.btnSearch.click();
+	    ImplicitExplictWaits.ImplicitWait(driver);
+		search.linkSearch.click();
+	    ImplicitExplictWaits.ImplicitWait(driver);	
+	    search.btnDownload.click();
+
+	}
+	@After
+	public void tearDown(){
+		System.out.println("run tearDown method");
+		//driver.quit();
+	}
+	
+	
+}
